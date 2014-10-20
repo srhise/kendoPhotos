8b98372fbfb183e401624db1c97d58811e7ac621
Sean Hise <srhise@Seans-MacBook-Pro.local>
2014-10-20 16:14:04 -0400

    store photo locally only if it exists

--- KendoPhotos/scripts/users.js
+++ KendoPhotos/scripts/users.js
@@ -20,7 +20,9 @@ app.Users = (function () {
             .then(function (data) {
 
                 var currentUserData = data.result;
-                currentUserData.PictureUrl = app.helper.resolveProfilePictureUrl(currentUserData.Picture);
+                if (currentUserData.Picture) {
+                    currentUserData.PictureUrl = app.helper.resolveProfilePictureUrl(currentUserData.Picture);
+                }                
                 currentUser.set('data', currentUserData);
 
                 // Get the data about all registered users
